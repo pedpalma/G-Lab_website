c044e1110e90c0cd2494de17584841ebc4f1cc92
pedpalma <pedro.francisco.palma@gmail.com>
2026-05-04 16:24:31 -0300

    feat: bubbles effect, bubbles images, removed custom cursor, contract im footer

--- assets/js/heroSlider.js
+++ assets/js/heroSlider.js
@@ -1,6 +1,12 @@
 // Lista de imagens do slider.
 
-const SLIDES = ["./assets/images/background-portrait.jpg"];
+const SLIDES = [
+  "./assets/images/background-portrait.jpg",
+  "./assets/images/max-500mb.png",
+  "./assets/images/max-650mb.png",
+  "./assets/images/max-800mb.png",
+  "./assets/images/vantage-club.png",
+];
 
 /** Intervalo entre as trocas em milissegundos */
 const INTERVAL = 5000;
